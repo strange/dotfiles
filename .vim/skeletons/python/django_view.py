3ff9c0fa62d2f6090152e52365004079ee203df3
Gustaf Sjöberg <gs@pipsq.com>
2013-11-05 11:11:26 +0100

    Updated django skels.

--- .vim/skeletons/python/django_view.py
+++ .vim/skeletons/python/django_view.py
@@ -2,7 +2,7 @@ from django import http
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
-from django.views.generic.simple import direct_to_template as render
+from django.shortcuts import render
 
 def view(request):
     return render(request, 'template.html', {
