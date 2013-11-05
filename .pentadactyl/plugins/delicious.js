5e3552db40ca4fd8ebc2249116c3a1709a9ed883
Gustaf Sjöberg <gs@pipsq.com>
2013-11-05 11:09:35 +0100

    Updated penta settings.

--- .pentadactyl/plugins/delicious.js
+++ .pentadactyl/plugins/delicious.js
@@ -5,16 +5,45 @@ group.commands.add(['dmark'], "Bookmark page on Delicious",
             '&url=', encodeURIComponent(buffer.URL),
             '&description=', encodeURIComponent(buffer.title),
             '&shared=no',
+            '&replace=yes',
             '&tags=', encodeURIComponent(args.join(','))
         ];
+        dactyl.echo("1");
         url = url.join('');
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', url, false);
-        xhr.send(null);
-        if (xhr.status != 200) {
-            dactyl.error("Error creating bookmark");
-        } else {
-            dactyl.echo("Bookmark added");
+        dactyl.echo("2");
+
+try {
+    util.httpGet(url, {
+        method: "POST",
+        onload: function (req) {
+            var status = req.responseXML.getElementsByTagName('result')[0].getAttribute('code');
+
+            if(status == "done") {
+                dactyl.echomsg("Worked");
+            } else {
+                dactyl.echomsg(status);
+            }
         }
+    });
+} catch (e) {
+    dactyl.echomsg(e);
+}
+
+dactyl.echo("3");
+
+
+
+        // var xhr = new XMLHttpRequest();
+        // dactyl.echo("3");
+        // xhr.open('POST', url, false);
+        // dactyl.echo("4");
+        // xhr.send(null);
+        // dactyl.echo("Before post");
+        // if (xhr.status != 200) {
+        //     dactyl.echo("Dude status");
+        //     // dactyl.error("Error creating bookmark");
+        // } else {
+        //     dactyl.echo("Bookmark added");
+        // }
     }
 );
